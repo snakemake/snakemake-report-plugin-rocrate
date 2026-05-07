20e753db5ae3abf260b4f6b37ac970f27eb97d67
Felix Bartusch <felix.bartusch@uni-tuebingen.de>
2026-05-07 15:55:06 +0200

    chore: Add pytest-cov dependency, add pytest stuff to .gitignore

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -5,7 +5,7 @@ from snakemake_interface_report_plugins.settings import ReportSettingsBase
 
 # Check out the base classes found here for all possible options and methods:
 # https://github.com/snakemake/snakemake/blob/main/src/snakemake/common/tests/__init__.py
-class TestWorkflowsBase(snakemake.common.tests.TestReportBase):
+class TestWorkflowsBase(snakemake.common.tests.TestReportBase): # type: ignore[misc]
     __test__ = True
 
     def get_reporter(self) -> str:
@@ -13,4 +13,4 @@ class TestWorkflowsBase(snakemake.common.tests.TestReportBase):
 
     def get_report_settings(self) -> Optional[ReportSettingsBase]:
         # instantiate ReportSettings of this plugin as appropriate
-        ...
+        return None
